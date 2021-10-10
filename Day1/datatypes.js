4e97f316fc0e77e795b85d0962cccd18d731b3d3
nouf1999 <70525803+nouf1999@users.noreply.github.com>
2021-10-10 03:36:25 +0000

    Days1 finish

---
+++ Day1/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof firstName) // string
+console.log(typeof lastName) // number
+console.log(typeof age) // boolean
+console.log(typeof valid) // object type
+// undefined
---
+++ Day1/variable.js
@@ -0,0 +1,38 @@
+// Question 4
+
+let firstName = "KAGONE"
+let valid=true
+let lastName =undefined
+let age=null
+// question 6
+let email
+let mobile_number
+let address
+let hometown
+
+// Question 7
+
+let my_name = "Touwinde Noufou KAGONE"
+let my_email="nouf35.com@gmail.com"
+let my_mobile_number="0022674283182"
+let myGithubUsername="nouf1999"
+
+//Question 8
+
+let my_first_name ="KAGONE"
+let my_last_name ="Touwinde Noufou"
+let my_marital_status="single"
+let my_country= "Burkina Faso"
+let my_age=22
+
+//Question 9
+
+let my_first_name ="KAGONE"; let my_last_name ="Touwinde Noufou";let my_marital_status="single";let my_country= "Burkina Faso";let my_age=22
+
+//Question 10
+
+let myAge = 22
+let yourAge = 23
+
+console.log("I am", myAge,"years old.")
+console.log("You are", yourAge,"years old.")
